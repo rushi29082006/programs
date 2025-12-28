61efdf8eb004e47d4062953f568eb8df725cc13e
siva rushinadh <karanamrushi834@gmail.com>
2025-12-28 09:37:18 +0530

    Add files via upload

---
+++ tim.java
@@ -0,0 +1,12 @@
+class kom{
+static double Main(double x,double y){
+
+return x+y;
+}
+static float Main(float a,float b){
+return a+b;
+}
+static void main(String... args){
+System.out.println(Main(5.00,4.07));
+}
+}
