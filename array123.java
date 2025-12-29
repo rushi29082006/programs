9a8b2f5b918ed5ce8f6802eef1a2e5cd51c5d040
siva rushinadh <karanamrushi834@gmail.com>
2025-12-29 21:32:02 +0530

    Add files via upload

---
+++ array123.java
@@ -0,0 +1,23 @@
+public class array123 {
+    static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        int[] ar=new int[arr.length];
+        int v=0;
+        for(int i:arr){
+            if(i%2==0){
+            ar[v]=i;
+            v+=1;
+            }
+            }
+            for(int j:ar){
+        System.out.println(j +" ");
+            }
+            }
+        
+            }
+            
+        
+    
+    
+    
+
