ef1f674eaf3de6e8826fd9ecc2106437a1fedffc
siva rushinadh <karanamrushi834@gmail.com>
2026-01-01 15:53:39 +0530

    Add files via upload

---
+++ jimm.java
@@ -0,0 +1,12 @@
+class jimm{
+static void main(String... args){
+    int a=20;
+    float b=a;
+    char c ='a';
+    String s="hello";
+    Byte n=011;
+    double d=(int)a;
+    System.out.println(a+b+c+s+n);
+    System.out.println(d);
+}
+}
