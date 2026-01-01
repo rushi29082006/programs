4d39e51fd1a211a7bec3e557b413dd4bf21ac9d6
siva rushinadh <karanamrushi834@gmail.com>
2026-01-01 18:52:57 +0530

    Add files via upload

---
+++ lit.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class lit{
+static void main(String arg[]){
+String s="jam";
+String h="jam";
+String g=new String("jam");
+System.out.println(s==g);
+System.out.println(g=="jom");
+
+System.out.println(Integer.MAX_VALUE);
+}
+}
